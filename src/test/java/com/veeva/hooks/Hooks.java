bb0aa82641be39692a565162b4574d7bce804053
Varshitha <151738993+Varshitharaodugyala@users.noreply.github.com>
2026-03-18 21:43:42 +0530

    removeed ionvertoryrunner

--- src/test/java/com/veeva/hooks/Hooks.java
+++ src/test/java/com/veeva/hooks/Hooks.java
@@ -28,4 +28,3 @@ public class Hooks {
                 scenario.getName(), scenario.getStatus());
     }
 }
-
--- src/test/java/com/veeva/runners/TestRunner.java
+++ src/test/java/com/veeva/runners/TestRunner.java
@@ -1,15 +1,20 @@
 package com.veeva.runners;
 
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.veeva",
-        plugin = {"pretty","html:target/cucumber-report.html"}
+        glue = {"com.veeva.stepdefinitions", "com.veeva.hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/report.html",
+                "json:target/cucumber.json",
+                "junit:target/cucumber-reports/junit-report.xml"
+        },
+        monochrome = true
 )
-
 public class TestRunner {
 }
