023d11bb6e68442d1681a972141b026910550b33
Varshitha <151738993+Varshitharaodugyala@users.noreply.github.com>
2026-03-18 21:14:16 +0530

    added POJO models Pet.java,Category.java,User.java in models

---
+++ src/main/java/com/veeva/models/Pet.java
@@ -0,0 +1,26 @@
+package com.veeva.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Pet {
+    private long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private String status;
+
+    public Pet() {}
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+    public Category getCategory() { return category; }
+    public void setCategory(Category category) { this.category = category; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public List<String> getPhotoUrls() { return photoUrls; }
+    public void setPhotoUrls(List<String> photoUrls) { this.photoUrls = photoUrls; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
