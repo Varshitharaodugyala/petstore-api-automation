a73a7056af4fcb8bfc2b56a55f59bd86793cd20e
Varshitha <151738993+Varshitharaodugyala@users.noreply.github.com>
2026-03-18 21:25:33 +0530

    upgraded Hooks with ScenarioContext injection and Log4J logging

--- src/test/java/com/veeva/hooks/Hooks.java
+++ src/test/java/com/veeva/hooks/Hooks.java
@@ -1,17 +1,31 @@
 package com.veeva.hooks;
 
-import io.cucumber.java.Before;
+import com.veeva.context.ScenarioContext;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Hooks {
 
+    private static final Logger log = LogManager.getLogger(Hooks.class);
+    private final ScenarioContext scenarioContext;
+
+    public Hooks(ScenarioContext scenarioContext) {
+        this.scenarioContext = scenarioContext;
+    }
+
     @Before
-    public void setUp() {
-        System.out.println("Starting test scenario...");
+    public void setUp(Scenario scenario) {
+        log.info("===== Starting: {} =====", scenario.getName());
     }
 
     @After
-    public void tearDown() {
-        System.out.println("Test scenario finished.");
+    public void tearDown(Scenario scenario) {
+        scenarioContext.clear();
+        log.info("===== Finished: {} | Status: {} =====",
+                scenario.getName(), scenario.getStatus());
     }
 }
+
